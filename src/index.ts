30d61decd6958d24d1f7cff90db8f52999ea3a01
Glauco Pantoni <glaucopantoni@gmail.com>
2022-11-18 14:36:19 -0300

    adds server and router

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, { json } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import router from './routers/router.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(json());
+
+app.use(router);
+
+export default app;
